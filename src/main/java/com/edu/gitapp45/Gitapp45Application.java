e7f4bfb7b6ac0035fd4273135c85a25fcd625895
ASUS-HJ\User <alswl1409@gmail.com>
2023-04-24 20:49:22 +0900

    first

---
+++ src/main/java/com/edu/gitapp45/Gitapp45Application.java
@@ -0,0 +1,13 @@
+package com.edu.gitapp45;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitapp45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitapp45Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/gitapp45/Gitapp45ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.gitapp45;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitapp45ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
